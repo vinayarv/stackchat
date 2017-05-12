81e7d9342621aa876d89322417e99ccbfe38ca33
Thomas Kelly <tmkelly28@gmail.com>
2017-05-12 11:31:53 -0400

    added semicolon

--- client/components/Main.js
+++ client/components/Main.js
@@ -3,7 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 import MessagesList from './MessagesList';
-import NewChannelEntry from './NewChannelEntry'
+import NewChannelEntry from './NewChannelEntry';
 
 export default class Main extends Component {
 
