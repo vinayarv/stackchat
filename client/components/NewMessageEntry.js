036a3dc47ad7b7e02a3c637f5778600d6a2c2f70
Thomas Kelly <tmkelly28@gmail.com>
2017-05-24 10:14:49 -0400

    remove some starting code from newmessageentry

--- client/components/NewMessageEntry.js
+++ client/components/NewMessageEntry.js
@@ -2,34 +2,14 @@ import React, { Component } from 'react';
 
 export default class NewMessageEntry extends Component {
 
-  constructor () {
-    super();
-    this.state = { content: '' };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange (evt) {
-    this.setState({ content: evt.target.value });
-  }
-
-  handleSubmit (evt) {
-    evt.preventDefault();
-    console.log('TODO: send this message: ', this.state.content);
-    this.setState({ content: '' });
-  }
-
   render () {
     return (
-      <form id="new-message-form" onSubmit={this.handleSubmit}>
+      <form id="new-message-form">
         <div className="input-group input-group-lg">
           <input
             className="form-control"
             type="text"
             name="content"
-            value={this.state.content}
-            onChange={this.handleChange}
             placeholder="Say something nice..."
           />
           <span className="input-group-btn">
