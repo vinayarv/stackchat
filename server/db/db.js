87946f992409505f89ef33c7e74d64e6ef97d818
Thomas Kelly <tmkelly28@gmail.com>
2017-06-05 17:45:40 -0400

    update to correct db name

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-  'postgres://localhost:5432/stackbook', {
+  'postgres://localhost:5432/stackchat', {
     logging: false
   }
 );
